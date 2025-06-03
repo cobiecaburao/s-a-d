35301d2301ddaff5f8bc4916f26ca7c7d12d7dd4
cobiecaburao <98910948+cobiecaburao@users.noreply.github.com>
2025-06-02 20:24:52 -0600

    Add files via upload

---
+++ Script/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+
+    private float xPosLastFrame;
+    // Update is called once per frame
+    void Update()
+    {
+        // Processing inputs
+        ProcessInputs();
+        flipCharacterX()
+    }
+
+    void FixedUpdate()
+    {
+        // Physics calculations
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY);
+
+        if (moveX != 0 || moveY !=0)
+        {
+            animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
+
+        bool flipped = moveDirection.x < 0;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f));
+
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        
+    }
+
+    void flipCharacterX()
+    {
+
+    }
+}
